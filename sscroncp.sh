85e58be68cd1b2c85933dd4ce2efe8d22880f465
echoe <sfelshman@gmail.com>
2016-09-14 09:55:30 -0500

    fix
    fix filepath

--- sscroncp.sh
+++ sscroncp.sh
@@ -1,6 +1,6 @@
 #This installs and starts sys-snap (cPanel version), changes the interval, and sets up a cronjob.
 echo "Installing cPanel sys-snap2. Please note this is not technically supported.";
-su - -c "wget --no-check-certificate -N -P /root/ http://echoe.github.com/etcediting/sys-snap-cpanelperl.pl;"
+su - -c "wget --no-check-certificate -N -P /root/ https://raw.githubusercontent.com/echoe/sscron/master/sys-snap-cpanelperl.pl;"
 chmod 744 /root/sys-snap-cpanelperl.pl;
 cd /root/; yes | ./sys-snap-cpanelperl.pl --start;
 echo "00 0 * * * yes | /usr/bin/perl /root/sys-snap-cpanelperl.pl --stop" > /etc/cron.d/syssnap;
